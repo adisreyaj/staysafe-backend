dde6071596745429ca8d28e8dae2e566ab9d1aa6
Adithya Sreyaj <adi.sreyaj@gmail.com>
2020-04-05 18:53:17 +0530

    🔒 Add security middlewares

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@
  * File Created: Sunday, 5th April 2020 1:45:13 pm
  * Author: Adithya Sreyaj
  * -----
- * Last Modified: Sunday, 5th April 2020 3:01:31 pm
+ * Last Modified: Sunday, 5th April 2020 6:45:31 pm
  * Modified By: Adithya Sreyaj<adi.sreyaj@gmail.com>
  * -----
  */
@@ -12,9 +12,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { AppConfiguration } from './core/config/app/app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  AppConfiguration.setup(app);
   app.setGlobalPrefix('api/v1');
   const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT') ?? 3000);
