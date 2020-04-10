3aa00f0c88086af84b1a71d6633364330aca9464
Adithya Sreyaj <adi.sreyaj@gmail.com>
2020-04-11 01:25:09 +0530

    💚  Fixing build issue related to configuration decryption

--- src/decrypt.js
+++ src/decrypt.js
@@ -4,12 +4,13 @@
  * File Created: Saturday, 11th April 2020 12:18:32 am
  * Author: Adithya Sreyaj
  * -----
- * Last Modified: Saturday, 11th April 2020 1:01:35 am
+ * Last Modified: Saturday, 11th April 2020 1:19:30 am
  * Modified By: Adithya Sreyaj<adi.sreyaj@gmail.com>
  * -----
  */
 
 const CryptoJS = require('crypto-js');
+const path = require('path');
 const fs = require('fs').promises;
 const dotenv = require('dotenv');
 
@@ -17,7 +18,7 @@ dotenv.config();
 (async () => {
   try {
     const ciphertext = (
-      await fs.readFile('./firebase_config.staysafe')
+      await fs.readFile(path.join(__dirname, './firebase_config.staysafe'))
     ).toString();
 
     const decryptedWords = CryptoJS.AES.decrypt(
@@ -25,7 +26,10 @@ dotenv.config();
       process.env.ENCRYPT_KEY,
     );
     const config = JSON.parse(decryptedWords.toString(CryptoJS.enc.Utf8));
-    await fs.writeFile('./firebase_config.json', JSON.stringify(config));
+    await fs.writeFile(
+      path.join(__dirname, './firebase_config.json'),
+      JSON.stringify(config),
+    );
     process.exit(0);
   } catch (error) {
     console.error(error);
--- src/encrypt.js
+++ src/encrypt.js
@@ -4,13 +4,14 @@
  * File Created: Saturday, 11th April 2020 12:18:32 am
  * Author: Adithya Sreyaj
  * -----
- * Last Modified: Saturday, 11th April 2020 12:37:06 am
+ * Last Modified: Saturday, 11th April 2020 1:20:08 am
  * Modified By: Adithya Sreyaj<adi.sreyaj@gmail.com>
  * -----
  */
 
 const CryptoJS = require('crypto-js');
 const fs = require('fs').promises;
+const path = require('path');
 const config = require('./firebase_config.json');
 const dotenv = require('dotenv');
 
@@ -21,7 +22,10 @@ dotenv.config();
       JSON.stringify(config),
       process.env.ENCRYPT_KEY,
     ).toString();
-    await fs.writeFile('./firebase_config.staysafe', ciphertext);
+    await fs.writeFile(
+      path.join(__dirname, './firebase_config.staysafe'),
+      ciphertext,
+    );
     process.exit(0);
   } catch (error) {
     console.error(error);
